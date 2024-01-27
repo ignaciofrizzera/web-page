b1edfc18ae738763990b37e3fc0063997c8b08a9
IgnacioFrizzera <ignaciofrizzera@gmail.com>
2024-01-27 20:41:38 -0300

    we do 8 days of data to avoid completely the 2mb limit (200kb ~ per day * 8 < 2mb)

--- src/data/spotify-etl-data.tsx
+++ src/data/spotify-etl-data.tsx
@@ -9,5 +9,5 @@ export const SPOTIFY_ETL_DATA = {
     },
     projectOverview: "Basic ETL developed in Python, using Spotify's API with the Spotipy package. The data is collected three times a day: 9:30AM, 5:30PM and 23:30PM (UTC-3). The ETL runs on AWS services such as Step-Functions, Lambda Functions, and S3.",
     dataVisualization: "The data is displayed in a calendar-alike form, where each box (day) represents my most played song for that date. I thought this was more interactive and fun to see.",
-    scope: "This page just displays the data for the last 12 days. I had to reduce the amount of days shown quite a bit due to the limits of the request and response bodies sizes of Next.js API routes. This could be improved by calculating the most listened song in the ETL's Load function, instead than here in the frontend, therefore not requering the entire music activity data."
+    scope: "This page just displays the data for the last 8 days. I had to reduce the amount of days shown quite a bit due to the limits of the request and response bodies sizes of Next.js API routes. This could be improved by calculating the most listened song in the ETL's Load function, instead than here in the frontend, therefore not requering the entire music activity data."
 } as const;
