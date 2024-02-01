2b8afb0e463295f360c35fc4dd44564f3b5ff3c3
IgnacioFrizzera <ignaciofrizzera@gmail.com>
2024-02-01 11:20:00 -0300

    we keep trying

--- src/app/api/songs/route.ts
+++ src/app/api/songs/route.ts
@@ -1,6 +1,7 @@
 import { S3Service } from "@/services/s3Service";
+import { NextResponse } from "next/server";
 
-// This actually disables caching.
+// This actually does not disable caching.
 export const fetchCache = 'force-no-store'
 export const revalidate = 0
 export const dynamic = 'force-dynamic'
@@ -9,7 +10,9 @@ export async function GET(request: Request) {
   try {
     const s3Service = new S3Service();
     const data = await s3Service.getData();
-    return Response.json(data);
+    const response = new NextResponse(JSON.stringify(data))
+    response.headers.set('Cache-Control', 'no-store');
+    return response;
   } catch (error) {
     console.error(error);
   }
